1b9a9c8663da98226ab15ae320a19232d8b3b2d1
Ariadna Perez <aperezgarrig@ukaachen.de>
2026-02-17 13:55:11 +0100

    Created Prescription filters

--- Exercice_Django/medical/filters.py
+++ Exercice_Django/medical/filters.py
@@ -1,6 +1,9 @@
 import django_filters
 
-from .models import Patient, Medication
+from .models import Patient, Medication, Prescription
+
+
+# OopCompanion:suppressRename
 
 
 class PatientFilter(django_filters.FilterSet):
@@ -34,3 +37,24 @@ class MedicationFilter(django_filters.FilterSet):
     class Meta:
         model = Medication
         fields = ["code", "label", "status"]
+
+
+class PrescriptionFilter(django_filters.FilterSet):
+    status = django_filters.CharFilter(field_name="status", lookup_expr="exact")
+    patient = django_filters.NumberFilter(field_name="patient_id", lookup_expr="exact")
+    medication = django_filters.NumberFilter(field_name="medication_id", lookup_expr="exact")
+
+    start_date = django_filters.DateFilter(field_name="start_date")
+    end_date = django_filters.DateFilter(field_name="end_date")
+
+    start_date__gte = django_filters.DateFilter(field_name="start_date", lookup_expr="gte")
+    start_date__lte = django_filters.DateFilter(field_name="start_date", lookup_expr="lte")
+
+    end_date__gte = django_filters.DateFilter(field_name="end_date", lookup_expr="gte")
+    end_date__lte = django_filters.DateFilter(field_name="end_date", lookup_expr="lte")
+
+    class Meta:
+        model = Prescription
+        fields = ["patient", "medication",
+                  "start_date", "start_date__gte", "start_date__lte",
+                  "end_date", "end_date__gte", "end_date__lte", "status"]
